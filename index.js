927f5b0c8d08450320352395eea185643949ad09
Aashitichavda11 <aashitichavda97@gmail.com>
2021-10-01 10:04:41 +0530

    commit message

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+import {appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+add line here 
